08d94d67fe677f1700da29e80280bdf8e0122290
Tyler Bristol <tahbristol@gmail.com>
2017-06-16 04:04:50 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,4 +18,17 @@ class PostsController < ApplicationController
 	  @post.save
 	  redirect_to post_path(@post)
 	end
+
+	def edit
+		@post = Post.find_by_id(params[:id])
+	end
+
+	def update
+		@post = Post.find(params[:id])
+
+		@post.update(title: params[:title]) unless params[:title].empty?
+		@post.update(description: params[:description]) unless params[:description].empty?
+		redirect_to post_path(@post)
+	end
+
 end
