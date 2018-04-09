09ae1390d9d21f376263aa3032b37d074177e4ea
adam n <adam.nawroc@gmail.com>
2018-04-09 13:57:57 +0000

    progress

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   resources :posts, only: [:index, :show, :new, :create]
+
+  # why arent we just throwing an edit option up above ??
+  # mayb they said it beforehand.
+  get 'posts/:id/edit', to: 'posts#edit', as: :edit_post
+  # why not patch?
+  put 'posts/:id', to: 'posts#update'
+
+
 end
