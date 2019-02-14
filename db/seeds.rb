000a0d5a93e00c4fb400e760be64547cd80e5f22
kenkuts <kennethchoi19@gmail.com>
2019-02-13 20:39:38 -0500

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Article.create(title: "The Smiths",description: "There is a light that never goes out")
+
+Article.create(title: "The Pixies",description: "Hey")
+
+Article.create(title: "Weezer",description: "Hash Pipe")
+
+Article.create(title: "The strokes",description: "Someday")
+
+Article.create(title: "Modest Mouse",description: "Trailer Trash")
+
+Article.create(title: "Joji",description: "Attention")
